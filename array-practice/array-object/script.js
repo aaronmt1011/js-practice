ada71d7000864c6f6a2cf66079cf966c850262a1
aaronmt1011 <aaronmt1011@gmail.com>
2023-05-02 13:16:02 -0600

    Updated arrays to include multiple array objects

--- array-practice/array-object/script.js
+++ array-practice/array-object/script.js
@@ -1,6 +1,47 @@
+/* 
+
+This is an example of an array with a single array object.
+
+*/
+/*
 let car = {
     "color": "purple",
     "type": "minivan",
     "regristration": new Date('2012-02-03'),
     "capacity": 7
 }
+*/
+
+
+/* 
+
+This is an example of an array with a multiple array object.
+
+*/
+
+let cars = [
+    {
+        "color": "purple",
+        "type": "minivan",
+        "regristration": new Date('2012-02-03'),
+        "capacity": 7
+    },
+    {
+        "color": "red",
+        "type": "station wagon",
+        "regristration": new Date('2018-03-03'),
+        "capacity": 6
+    },
+    {
+        "color": "blue",
+        "type": "sedan",
+        "regristration": new Date('2017-01-03'),
+        "capacity": 5
+    },
+    {
+        "color": "orange",
+        "type": "",
+        "regristration": new Date('2014-08-04'),
+        "capacity": 7
+    }
+]
