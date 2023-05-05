6c0d6103fe90e4bdcbcd57b1cd26c3cddbf28603
aaronmt1011 <aaronmt1011@gmail.com>
2023-05-05 17:00:31 -0600

    Used functions that can add stuff on cars array

--- array-practice/array-object/script.js
+++ array-practice/array-object/script.js
@@ -40,8 +40,49 @@ let cars = [
     },
     {
         "color": "orange",
-        "type": "",
+        "type": "truck",
         "regristration": new Date('2014-08-04'),
-        "capacity": 7
+        "capacity": 6
     }
 ]
+
+let car = {
+    "color": "pink",
+    "type": "suv",
+    "regristration": new Date('2016-01-03'),
+    "capacity": 7
+}
+
+let car1 = {
+    "color": "yellow",
+    "type": "electric car",
+    "regristration": new Date('2016-01-03'),
+    "capacity": 5
+}
+
+let car2 = {
+    "color": "brown",
+    "type": "semi-truck",
+    "regristration": new Date('2004-04-02'),
+    "capacity": 5
+}
+
+/*
+
+Code below are various functions that can be used with array objects
+
+*/
+
+ 
+// .unshift() adds an object at the first position of the array 
+cars.unshift(car);
+
+
+// .push() adds an object to the last position of the array
+cars.push(car1);
+
+
+// .splice() can be used to add an object to any position of the array
+cars.splice(2, 0, car2);
+console.log(cars);
+
